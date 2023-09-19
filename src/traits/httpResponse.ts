7312ef63d5bb70a377b0fa12a12274b75b32d6b2
ThantHtetAung101 <galactuscosmic101@gmail.com>
2023-09-19 17:45:53 +0630

    CORS issues solved (i guess :3)

--- src/traits/httpResponse.ts
+++ src/traits/httpResponse.ts
@@ -1,17 +1,25 @@
 class HttpResponse {
     success (data: any, message: String) {
-        return {
+        let res = new Response(JSON.stringify({
             'success': true,
             'message': message,
             'data': data,
-        }
+        }))
+        res.headers.set('Access-Control-Allow-Origin', '*');
+        res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.headers.set('Content-Type', 'application/json')
+        return res;
     }
     error (data: null, message: String) {
-        return {
+        let res = new Response(JSON.stringify({
             'success': false,
             'message': message,
             'data': data
-        }
+        }))
+        res.headers.set('Access-Control-Allow-Origin', '*');
+        res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.headers.set('Content-Type', 'application/json')
+        return res;
     }
 }
 export default HttpResponse;
